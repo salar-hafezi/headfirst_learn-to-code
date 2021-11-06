609516c6b91c8bde6214ff4882c5d572657f0dfc
Salar Hafezi <salar.hfz@gmail.com>
2021-11-06 18:53:55 +0330

    ch02 - dog age calculator

---
+++ ch02/dog_age_calc/app.py
@@ -0,0 +1,9 @@
+dog_name = input('What\'s your dog name?\n')
+dog_age = input('What\'s your dog age?\n')
+
+age_in_human_years = int(dog_age) * 7
+
+result = dog_name + ' is ' + str(age_in_human_years) + ' in human years!'
+
+print(result)
+
